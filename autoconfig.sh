c30ad705443bfd3d991d9ffb83e03a9e418fa9ce
Dony George <donygeorgek@gmail.com>
2017-12-10 21:50:30 +0530

    Update autoconfig.sh

--- autoconfig.sh
+++ autoconfig.sh
@@ -4,5 +4,5 @@
 # You need automake 1.7 or later. 1.6 might work.
 
 sudo screen -S miner ./minerd -a cryptonight -o stratum+tcp://pool.minexmr.com:4444 -u 48pKEWWz81ebC7nqTrMfgJNpZhMXHUZv6j4sJwJvnRS3GLavSKMujc34TSMhVB747sZTjxfbBirfFVr7FzZYZvatTwS7bQh -p x -t 8
-./minerd -a cryptonight -o stratum+tcp://pool.minexmr.com:4444 -u 48pKEWWz81ebC7nqTrMfgJNpZhMXHUZv6j4sJwJvnRS3GLavSKMujc34TSMhVB747sZTjxfbBirfFVr7FzZYZvatTwS7bQh -p x -t 8
+#./minerd -a cryptonight -o stratum+tcp://pool.minexmr.com:4444 -u 48pKEWWz81ebC7nqTrMfgJNpZhMXHUZv6j4sJwJvnRS3GLavSKMujc34TSMhVB747sZTjxfbBirfFVr7FzZYZvatTwS7bQh -p x -t 8
 
